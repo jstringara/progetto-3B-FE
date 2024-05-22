29080ed4c46d4ec77d031d80e736a9e9a1f0d014
jstringara <62561822+jstringara@users.noreply.github.com>
2024-05-22 10:43:33 +0200

    Fix wrong term

--- Plot/plot_C_front_next.m
+++ Plot/plot_C_front_next.m
@@ -1,9 +1,9 @@
 function plot_C_front_next(C_spread_front, C_spread_next, dates)
 
 figure;
-plot(common_dates, 100 * C_spread_front, 'b', 'LineWidth', 1.5);
+plot(dates, 100 * C_spread_front, 'b', 'LineWidth', 1.5);
 hold on
-plot(common_dates, 100 * C_spread_next, 'r', 'LineWidth', 1.5);
+plot(dates, 100 * C_spread_next, 'r', 'LineWidth', 1.5);
 legend('Front December', 'Next December');
 title('C-Spread for the front December and next December EUA futures');
 xlim([dates(1) - calmonths(6), dates(end)])
