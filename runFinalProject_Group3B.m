f22522683e6dee19b4e20de4ca9e155b3d2f7162
jstringara <62561822+jstringara@users.noreply.github.com>
2024-05-27 10:52:17 +0200

    Make main consistent with new functions

--- runFinalProject_Group3B.m
+++ runFinalProject_Group3B.m
@@ -23,6 +23,7 @@ addpath('Data');
 addpath('Preprocessing');
 addpath('Preprocessed')
 addpath('Bootstrap');
+addpath('Spreads');
 addpath('Python');
 addpath('Plot');
 
@@ -38,6 +39,9 @@ preprocessing; % run the preprocessing script
 
 % bootstrap the curves
 [dates, DF, zrates] = bootstrapCurves(OIS_Data);
+
+% fill in the zrates to match the front dates
+[dates, zrates] = fill_zrates(zrates, dates, Daily_Future.Date);
 
 % animated_zrates(zrates, dates)
 
@@ -77,7 +81,7 @@ grouping = [
 
 %% Point 3) compute the C-Spread for the EUA futures
 
-risk_free_rate = RiskFreeRate(dates, zrates, Daily_Future.Date);
+risk_free_rate = RiskFreeRate(dates, zrates, Front_December.Expiry);
 
 C_spread_front = compute_C_Spread(Front_December, Daily_Future, risk_free_rate);
 C_spread_next = compute_C_Spread(Next_December, Daily_Future, risk_free_rate);
@@ -134,83 +138,11 @@ disp(['The standard deviation of the C-Spread is: ', num2str(std_C_spread * 100)
 
 %% Compute the Z-Spread for each bond
 
-% if the z spreads file does not exist, compute the Z-Spreads
-if ~isfile('Z_Spreads.mat')
-
-% start a waitbar
-h = waitbar(0, 'Computing the Z-Spreads');
-tot = length(Bonds);
-
-% group the bonds by issuer
-Bonds_By_Issuer = struct;
-
-for i = 1:length(Bonds)
-    % compute the Z-Spread for the bond
-    Bonds{i}.Z_Spreads = compute_ZSpread(Bonds{i}, dates, zrates);
-    % update the waitbar
-    waitbar(i/tot, h, ['Computing the Z-Spreads: ', num2str(i/tot*100), '%'])
-    
-    % add the bond to the issuer in the struct
-    if isfield(Bonds_By_Issuer, Bonds{i}.Issuer)
-        Bonds_By_Issuer.(Bonds{i}.Issuer){end+1} = Bonds{i};
-    else
-        Bonds_By_Issuer.(Bonds{i}.Issuer) = {Bonds{i}};
-    end
-
-end
-% close the waitbar
-close(h);
-
-%% Compute the Z-Spread for each issuer
-
-
-    % create a table to store the Z-Spreads
-    Z_spread = table(Daily_Future.Date, zeros(size(Daily_Future.Date)), ...
-        'VariableNames', {'Date', 'Z_Spread'});
-
-  
-    total_issuers_active = zeros(height(Daily_Future), 1);
-
-    % iterate over the fields of the struct (the issuers)
-    for issuer = fields(Bonds_By_Issuer)'
-        % get the bonds of the issuer
-        bonds = Bonds_By_Issuer.(issuer{1});
-        % compute the total volume of bonds traded for each date
-        total_volume = zeros(height(Daily_Future), 1);
-        Z_spread_issuer = zeros(height(Daily_Future), 1);
-        for j = 1:length(bonds)
-            % exclude bond with code "XS0877820422" (it has NaN values)
-            if bonds{j}.Code == "XS0877820422"
-                continue
-            end
-            Z_spread_issuer = Z_spread_issuer + bonds{j}.Volume .* bonds{j}.Z_Spreads;
-            total_volume = total_volume + bonds{j}.Volume .* (bonds{j}.Z_Spreads ~= 0);
-        end
-        % normalize the Z-Spreads of the issuer by the total volume
-        Z_spread_issuer = Z_spread_issuer ./ total_volume;
-        % fill the NaN values (no bonds traded)
-        Z_spread_issuer(isnan(Z_spread_issuer)) = 0;
-        % add the Z-Spreads to the table
-        Z_spread.Z_Spread = Z_spread.Z_Spread + Z_spread_issuer;
-        % add the number of active issuers
-        total_issuers_active = total_issuers_active + (total_volume ~= 0);
-    end
-
-    % normalize the Z-Spreads by the number of issuers
-    Z_spread.Z_Spread = Z_spread.Z_Spread ./ total_issuers_active;
-    Z_spread.Z_Spread(isnan(Z_spread.Z_Spread)) = 0;
-
-    % save the Z-Spreads
-    save('Z_Spreads.mat', 'Z_spread');
-
-else
-    % load the Z-Spreads
-    load('Z_Spreads.mat');
-end
+Z_spread = compute_ZSpread(Bonds, dates, zrates);
 
 %% Plot the Z-Spread
 
-% plot_C_Z_r(C_spread, Z_spread, risk_free_rate)
+plot_C_Z_r(C_spread, Z_spread, risk_free_rate)
 
 %% Mean and Variance of the Z-Spread (only on dates before 2021)
 
