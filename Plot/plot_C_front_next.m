a33f628e2e84abd031ae4c40a53dce360db58b42
jstringara <62561822+jstringara@users.noreply.github.com>
2024-05-31 15:58:16 +0200

    Pad out plot

--- Plot/plot_C_front_next.m
+++ Plot/plot_C_front_next.m
@@ -12,7 +12,7 @@ plot(C_spread_next.Date, 100 * C_spread_next.C_Spread, 'r', 'LineWidth', 1.5);
 legend('Front December', 'Next December');
 title('C-Spread for the front December and next December EUA futures');
 % we bring back the low end of the x-axis to 6 months before the first date
-xlim([C_spread_front.Date(1) - calmonths(6), C_spread_front.Date(end)]);
+xlim([C_spread_front.Date(1) - calmonths(6), C_spread_front.Date(end)+calmonths(6)]);
 % have grid points each 0.5 of the y
 yticks(-0.5:0.5:3.5);
 ylim([-1, 5])
