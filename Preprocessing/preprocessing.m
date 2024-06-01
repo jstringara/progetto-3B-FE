6434ff2c3b997e17f7736f79a16c1341568a2523
jstringara <62561822+jstringara@users.noreply.github.com>
2024-06-01 15:23:42 +0200

    Fix the expiries of the futures

--- Preprocessing/preprocessing.m
+++ Preprocessing/preprocessing.m
@@ -30,12 +30,32 @@ Next_December = Next_December(ismember(Next_December.Date, dates), :);
 Next_2_December = Next_2_December(ismember(Next_2_December.Date, dates), :);
 Daily_Future = Daily_Future(ismember(Daily_Future.Date, dates), :);
 
-% last expiry for the front is 19th december 2022
+% Fix front expiries
 front_expiry = datetime(2022, 12, 19);
-next_expiry = datetime(2023, 12, 18);
 % adjust the expiry dates past the phase IV dates
 Front_December.Expiry(Front_December.Expiry == phase_IV_dates(2)) = front_expiry;
-Next_December.Expiry(Next_December.Expiry == phase_IV_dates(2)) = next_expiry;
+
+% adjust the Next expiry dates
+front_expiries = unique(Front_December.Expiry);
+third_from_last_expiry = front_expiries(end-2);
+penultimate_expiry = front_expiries(end-1);
+next_expiry = datetime(2023, 12, 18);
+% adjust the expiry of the next
+Next_December.Expiry(Next_December.Date >= third_from_last_expiry & Next_December.Date < penultimate_expiry) ...
+    = front_expiry;
+Next_December.Expiry(Next_December.Date >= penultimate_expiry) ...
+    = next_expiry;
+
+% adjust the Next 2 expiry dates
+fourth_from_last_expiry = front_expiries(end-3);
+next_2_expiry = datetime(2024, 12, 23);
+% adjust the expiry of the next
+Next_2_December.Expiry(Next_2_December.Date >= fourth_from_last_expiry & Next_2_December.Date < third_from_last_expiry) ...
+    = front_expiry;
+Next_2_December.Expiry(Next_2_December.Date >= third_from_last_expiry & Next_2_December.Date < penultimate_expiry) ...
+    = next_expiry;
+Next_2_December.Expiry(Next_2_December.Date >= penultimate_expiry) ...
+    = next_2_expiry;
 
 % preprocess the bonds data
 Bonds = preprocessBonds(phase_IV_dates(1), phase_IV_dates(2), dates);
