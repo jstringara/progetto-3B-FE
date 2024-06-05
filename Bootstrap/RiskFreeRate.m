3737de61274b001d5661c0b25f1dcef0bbc5dbf3
jstringara <62561822+jstringara@users.noreply.github.com>
2024-06-05 18:56:10 +0200

    Enforce flat extrapolation in matlab

--- Bootstrap/RiskFreeRate.m
+++ Bootstrap/RiskFreeRate.m
@@ -12,6 +12,12 @@ rfr = zeros(height(target_dates), 1);
 for i=1:height(target_dates)
     expiry = target_dates(i);
     rfr(i) = interp1(dates(i,2:end), zero_rates(i,:), expiry, 'linear', 'extrap');
+    % if the date is before the first date, use the first rate
+    if expiry < dates(i,2)
+        rfr(i) = zero_rates(i,1);
+    % if the date is after the last date, use the last rate
+    elseif expiry > dates(i,end)
+        rfr(i) = zero_rates(i,end);
 end
 
 end
