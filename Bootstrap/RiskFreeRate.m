92ecdaec7c899f6ccb4cc61c7436c380d09eaa6b
jstringara <62561822+jstringara@users.noreply.github.com>
2024-06-06 15:42:18 +0200

    use act 365 for interpolation of zero rates

--- Bootstrap/RiskFreeRate.m
+++ Bootstrap/RiskFreeRate.m
@@ -8,10 +8,13 @@ function rfr = RiskFreeRate(dates, zero_rates, target_dates)
 
 % interpolate the risk free rate for the needed expiry
 rfr = zeros(height(target_dates), 1);
+ACT_365 = 3;
 
 for i=1:height(target_dates)
     expiry = target_dates(i);
-    rfr(i) = interp1(dates(i,2:end), zero_rates(i,:), expiry, 'linear', 'extrap');
+    yf_expiry = yearfrac(dates(i,1), expiry, ACT_365);
+    yf_dates = yearfrac(dates(i,1), dates(i,2:end), ACT_365);
+    rfr(i) = interp1(yf_dates, zero_rates(i,:), yf_expiry, 'linear', 'extrap');
     % if the date is before the first date, use the first rate
     if expiry < dates(i,2)
         rfr(i) = zero_rates(i,1);
